dab7c7bd2a6d573ab9fb1331fbefed08d19a38e0
Enrico Nasca <enrico.nasca@uk-essen.de>
2024-04-12 13:10:50 +0000

    Introduce a new playbook for installing rke2
    It uses upstream roles from
    https://github.com/rancherfederal/rke2-ansible

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env sh
+set -eu
 
-# Install Ansible roles.
-ansible-galaxy install -r ansible/meta/requirements.yml
+SCRIPTDIR="$(dirname "$(readlink -fn "$0" )")"
+DEPS_DIR="$SCRIPTDIR/../ansible/deps"
+META_DIR="$SCRIPTDIR/../ansible/meta"
+
+# Check out the upstream repo.
+if [ ! -d "$DEPS_DIR" ]; then
+	mkdir "$DEPS_DIR"
+fi
+cd "$DEPS_DIR"
+
+if [ -d "rke2-ansible" ]; then
+	cd "rke2-ansible"
+	git pull
+else
+	git clone "https://github.com/rancherfederal/rke2-ansible.git"
+fi
+
+# Install upstream dependencies.
+ansible-galaxy collection install -r "$DEPS_DIR/rke2-ansible/requirements.yml"
+
+# Install dependencies.
+ansible-galaxy collection install -r "$META_DIR/requirements.yml"
